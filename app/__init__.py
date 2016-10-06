e994780401a1d2d56d7569e840ec4b9eb7d1600a
valdadyne <rodgers.gitau@gmail.com>
2016-10-06 18:51:25 +0300

    Index Page

--- app/__init__.py
+++ app/__init__.py
@@ -11,8 +11,12 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return render_template('dashboard.html')
+    return render_template('index.html')
 
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 @app.errorhandler(404)
 def not_found(error):
